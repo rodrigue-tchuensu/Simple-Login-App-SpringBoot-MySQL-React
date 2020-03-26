1a0f3830b5a30e840c64b6b5b3edb98a9b45c7dc
rodrigue-tchuensu <rodrigue.tchuensupouopse@gmail.com>
2020-03-26 21:05:33 +0100

    Spring initialisation startup project, local MySQL datasource configured

---
+++ spring-boot-server/src/main/java/de/tchuensu/home/springbootserver/SpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package de.tchuensu.home.springbootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-server/src/test/java/de/tchuensu/home/springbootserver/SpringBootServerApplicationTests.java
@@ -0,0 +1,15 @@
+package de.tchuensu.home.springbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+@SpringBootTest
+class SpringBootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
